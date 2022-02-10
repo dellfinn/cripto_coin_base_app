af49bf1e0826b4c3d9fbcece0cd7510901d3755f
Italo Ruan <paradoxonitalo@gmail.com>
2022-02-10 14:45:35 -0300

    Finalizando o projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:cripto_coin_base/screen/mainScreen.dart';
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'screen/coin_card/coinCard.dart';
+import 'screen/coin_model/coinModel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +15,80 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const Home(),
+      home: Home(),
     );
   }
 }
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  Future<List<Coin>> fetchCoin() async {
+    coinList = [];
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> values = [];
+      values = json.decode(response.body);
+      if (values.length > 0) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+            coinList.add(Coin.fromJson(map));
+          }
+        }
+        setState(() {
+          coinList;
+        });
+      }
+      return coinList;
+    } else {
+      throw Exception('Falha ao carregar as crypto moedas');
+    }
+  }
+
+  @override
+  void initState() {
+    fetchCoin();
+    Timer.periodic(const Duration(seconds: 10), (timer) => fetchCoin());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        title: Center(
+          child: Text(
+            'Cripto Moedas',
+            style: TextStyle(
+              color: Colors.grey[900],
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: coinList.length,
+        itemBuilder: (context, index) {
+          return CoinCard(
+            name: coinList[index].name,
+            symbol: coinList[index].symbol,
+            imageUrl: coinList[index].imageUrl,
+            price: coinList[index].price.toDouble(),
+            change: coinList[index].change.toDouble(),
+            changePercentage: coinList[index].changePercentage.toDouble(),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/screen/coin_card/coinCard.dart
+++ lib/screen/coin_card/coinCard.dart
@@ -1,50 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        backgroundColor: Colors.grey[300],
-        elevation: 2,
-        title: Center(
-          child: Text(
-            'Crypto Base',
-            style: TextStyle(
-              color: Colors.grey[900],
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          CoinCard(),
-        ],
-      ),
-    );
-  }
-}
-
 class CoinCard extends StatelessWidget {
-  const CoinCard({
-    Key? key,
-  }) : super(key: key);
+  CoinCard({
+    required this.name,
+    required this.symbol,
+    required this.imageUrl,
+    required this.price,
+    required this.change,
+    required this.changePercentage,
+  });
+
+  String name;
+  String symbol;
+  String imageUrl;
+  double price;
+  double change;
+  double changePercentage;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
       child: Container(
         height: 100,
         decoration: BoxDecoration(
@@ -58,7 +34,7 @@ class CoinCard extends StatelessWidget {
               spreadRadius: 1,
             ),
             BoxShadow(
-              color: Colors.grey,
+              color: Colors.white,
               offset: Offset(-4, -4),
               blurRadius: 10,
               spreadRadius: 1,
@@ -81,20 +57,18 @@ class CoinCard extends StatelessWidget {
                       spreadRadius: 1,
                     ),
                     BoxShadow(
-                      color: Colors.grey,
+                      color: Colors.white,
                       offset: Offset(-4, -4),
                       blurRadius: 10,
                       spreadRadius: 1,
                     ),
                   ],
                 ),
-                height: 68,
-                width: 68,
+                height: 60,
+                width: 60,
                 child: Padding(
                   padding: const EdgeInsets.all(10.0),
-                  child: Image.network(
-                    'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-                  ),
+                  child: Image.network(imageUrl),
                 ),
               ),
             ),
@@ -104,19 +78,22 @@ class CoinCard extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Bitcoin',
-                      style: TextStyle(
-                        color: Colors.grey[900],
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        name,
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                     Text(
-                      'btc',
+                      symbol,
                       style: TextStyle(
                         color: Colors.grey[900],
-                        fontSize: 28,
+                        fontSize: 20,
                       ),
                     ),
                   ],
@@ -130,25 +107,33 @@ class CoinCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Text(
-                    '36273',
+                    price.toDouble().toString(),
                     style: TextStyle(
                       color: Colors.grey[900],
-                      fontSize: 22,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   Text(
-                    '933.13',
+                    change.toDouble() < 0
+                        ? change.toDouble().toString()
+                        : '+' + change.toDouble().toString(),
                     style: TextStyle(
-                      color: Colors.grey[900],
+                      color: change.toDouble() < 0 ? Colors.red : Colors.green,
                       fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                   Text(
-                    '2.6%',
+                    changePercentage.toDouble() < 0
+                        ? changePercentage.toDouble().toString() + '%'
+                        : '+' + changePercentage.toDouble().toString() + '%',
                     style: TextStyle(
-                      color: Colors.grey[900],
+                      color: changePercentage.toDouble() < 0
+                          ? Colors.red
+                          : Colors.green,
                       fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
